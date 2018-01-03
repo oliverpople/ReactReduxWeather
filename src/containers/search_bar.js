5ff30ffd2698714be28f96873e5fd29009914065
oliverpople <oliverpople@gmail.com>
2018-01-03 11:04:39 +0000

    Initialised search_bar component/’s state with constructor(props) function and added event handler onInputChange.  Used the .bind(this) method to attached context to event handler onInputChange.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' }
+
+    this.onInputChnage = this.onInputChnage.bind(this);
+  }
+
+  onInputChnage(event) {
+    console.log(event.target.value);
+    this.setState({term: event.target.value});
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Get a five-day forecast in your favourite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChnage} />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
