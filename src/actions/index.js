c67add68b213d7fe32c254e55c913058b0cbe8f5
oliverpople <oliverpople@gmail.com>
2018-01-04 09:15:20 +0000

    Added ROOT_URL and fetchWeather function to index.js action.  fetchWeather takes a city name as an argument and returns a url requesting weather data for that city.  Also, installed package axios to make ajax requests.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 const API_KEY = '';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+const url = `${ROOT_URL}&q=${city},GB`;
+
+  return {
+    type: FETCH_WEATHER
+  };
+}
